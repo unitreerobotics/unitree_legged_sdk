a3a829aa09238fa760cc77c64ff0ffdd0378eae6
Bian Zekun <568574579@qq.com>
2020-07-14 20:02:49 +0800

    added a combined lcm server for low/high commands and A1/Aliengo

---
+++ examples/lcm_server.cpp
@@ -0,0 +1,60 @@
+#include "unitree_legged_sdk/lcm_server.h"
+#include <iostream>
+
+using namespace UNITREE_LEGGED_SDK;
+
+//argv[1]:robot name: A1 or Aliengo, not case sensitive
+//argv[2]:control level: LOWLEVEL or HIGHLEVEL, not case sensitive
+int main(int argc, char *argv[]) 
+{
+    LeggedType rname;
+    if(strcasecmp(argv[1], "A1") == 0)
+        rname = LeggedType::A1;
+    else if(strcasecmp(argv[1], "Aliengo") == 0)
+        rname = LeggedType::Aliengo;
+    else
+    {
+        std::cout << "Robot name error! Can only be A1 or Aliengo(not case sensitive)" << std::endl;
+        exit(-1);
+    }
+    
+    if(strcasecmp(argv[2], "LOWLEVEL") == 0)
+    {
+        Lcm_Server_Low server(rname);
+        server.mylcm.SubscribeCmd();
+        LoopFunc loop_control("control_loop", 0.002, boost::bind(&Lcm_Server_Low::RobotControl, &server));
+        LoopFunc loop_udpSend("UDP_Send", 0.002, 3, boost::bind(&Lcm_Server_Low::UDPSend, &server));
+        LoopFunc loop_udpRecv("UDP_Recv", 0.002, 3, boost::bind(&Lcm_Server_Low::UDPRecv, &server));
+        LoopFunc loop_lcm("LCM_Recv", 0.002, boost::bind(&Lcm_Server_Low::LCMRecv, &server));
+        loop_udpSend.start();
+        loop_udpRecv.start();
+        loop_lcm.start();
+        loop_control.start();
+        while(1){
+            sleep(10);
+        }
+    }
+    else if(strcasecmp(argv[2], "HIGHLEVEL") == 0)
+    {
+        Lcm_Server_High server(rname);
+        server.mylcm.SubscribeCmd();
+        LoopFunc loop_control("control_loop", 0.002, boost::bind(&Lcm_Server_High::RobotControl, &server));
+        LoopFunc loop_udpSend("UDP_Send", 0.002, 3, boost::bind(&Lcm_Server_High::UDPSend, &server));
+        LoopFunc loop_udpRecv("UDP_Recv", 0.002, 3, boost::bind(&Lcm_Server_High::UDPRecv, &server));
+        LoopFunc loop_lcm("LCM_Recv", 0.002, boost::bind(&Lcm_Server_High::LCMRecv, &server));
+        loop_udpSend.start();
+        loop_udpRecv.start();
+        loop_lcm.start();
+        loop_control.start();
+        while(1){
+            sleep(10);
+        }
+    }
+    else
+    {
+        std::cout << "Control level name error! Can only be highlevel or lowlevel(not case sensitive)" << std::endl;
+        exit(-1);
+    }
+
+    return 0; 
+}
---
+++ include/unitree_legged_sdk/lcm_server.h
@@ -0,0 +1,108 @@
+/************************************************************************
+Copyright (c) 2020, Unitree Robotics.Co.Ltd. All rights reserved.
+Use of this source code is governed by the MPL-2.0 license, see LICENSE.
+************************************************************************/
+
+#ifndef _UNITREE_LEGGED_LCM_SERVER_
+#define _UNITREE_LEGGED_LCM_SERVER_
+
+#include "comm.h"
+#include "unitree_legged_sdk/unitree_legged_sdk.h"
+
+namespace UNITREE_LEGGED_SDK
+{
+// Low command Lcm Server
+class Lcm_Server_Low
+{
+public:
+    Lcm_Server_Low(LeggedType rname) : control(rname, LOWLEVEL), udp(){
+        control.InitCmdData(cmd);
+    }
+    void UDPRecv(){
+        udp.Recv();
+    }
+    void UDPSend(){
+        udp.Send();
+    }
+    void LCMRecv();
+    void RobotControl() ;
+
+    Control control;
+    UDP udp;
+    LCM mylcm;
+    LowCmd cmd = {0};
+    LowState state = {0};
+};
+void Lcm_Server_Low::LCMRecv()
+{
+    if(mylcm.lowCmdLCMHandler.isrunning){
+        pthread_mutex_lock(&mylcm.lowCmdLCMHandler.countMut);
+        mylcm.lowCmdLCMHandler.counter++;
+        if(mylcm.lowCmdLCMHandler.counter > 10){
+            printf("Error! LCM Time out.\n");
+            exit(-1);              // can be commented out
+        }
+        pthread_mutex_unlock(&mylcm.lowCmdLCMHandler.countMut);
+    }
+    mylcm.Recv();
+}
+void Lcm_Server_Low::RobotControl() 
+{
+    udp.GetRecv(state);
+    mylcm.Send(state);
+    mylcm.Get(cmd);
+    udp.SetSend(cmd);
+}
+
+
+
+// High command Lcm Server
+class Lcm_Server_High
+{
+public:
+    Lcm_Server_High(LeggedType rname): control(rname, HIGHLEVEL), udp(){
+        control.InitCmdData(cmd);
+    }
+    void UDPRecv(){
+        udp.Recv();
+    }
+    void UDPSend(){
+        udp.Send();
+    }
+    void LCMRecv();
+    void RobotControl();
+    
+    Control control;
+    UDP udp;
+    LCM mylcm;
+    HighCmd cmd = {0};
+    HighState state = {0};
+};
+
+void Lcm_Server_High::LCMRecv()
+{
+    if(mylcm.highCmdLCMHandler.isrunning){
+        pthread_mutex_lock(&mylcm.highCmdLCMHandler.countMut);
+        mylcm.highCmdLCMHandler.counter++;
+        if(mylcm.highCmdLCMHandler.counter > 10){
+            printf("Error! LCM Time out.\n");
+            exit(-1);              // can be commented out
+        }
+        pthread_mutex_unlock(&mylcm.highCmdLCMHandler.countMut);
+    }
+    mylcm.Recv();
+}
+
+void Lcm_Server_High::RobotControl() 
+{
+    udp.GetRecv(state);
+    mylcm.Send(state);
+    mylcm.Get(cmd);
+    udp.SetSend(cmd);
+}
+
+
+
+
+}
+#endif
