515101973b36aedcb726abfffaa3b31e220822a9
TrivasZhang <zhangygdyx@126.com>
2020-08-24 10:07:49 +0800

    add comments

--- include/unitree_legged_sdk/quadruped.h
+++ include/unitree_legged_sdk/quadruped.h
@@ -19,7 +19,7 @@ enum class HighLevelType {
 	Sport
 };
 
-void InitEnvironment();
+void InitEnvironment();      // memory lock
 
 // definition of each leg and joint
 constexpr int FR_ = 0;       // leg index
