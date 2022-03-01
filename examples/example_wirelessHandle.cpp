c449afe07240f9f39143bd9eafe972de70882afa
TrivasZhang <zhangygdyx@126.com>
2022-03-01 16:42:06 +0800

    example use array

--- examples/example_wirelessHandle.cpp
+++ examples/example_wirelessHandle.cpp
@@ -4,7 +4,7 @@ Use of this source code is governed by the MPL-2.0 license, see LICENSE.
 ************************************************************************/
 
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
-#include "unitree_legged_sdk/unitree_joystick.h"
+#include "unitree_legged_sdk/joystick.h"
 #include <math.h>
 #include <iostream>
 #include <unistd.h>
@@ -45,7 +45,7 @@ void Custom::RobotControl()
     motiontime++;
     udp.GetRecv(state);
 
-    memcpy(&_keyData, state.wirelessRemote, 40);
+    memcpy(&_keyData, &state.wirelessRemote[0], 40);
 
     if((int)_keyData.btn.components.A == 1){
         std::cout << "The key A is pressed, and the value of lx is " << _keyData.lx << std::endl;
